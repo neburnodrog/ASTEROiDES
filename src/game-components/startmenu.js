142aa52292585364e81b3e1ede4bd4b2600aa196
Ruben Karlsson <neburgordon@gmail.com>
2021-04-09 10:39:10 +0200

    typo

--- src/game-components/startmenu.js
+++ src/game-components/startmenu.js
@@ -22,7 +22,7 @@ export class StartMenu {
         p5.fill("#AFE4FF");
         p5.textAlign(p5.CENTER, p5.CENTER)
         p5.translate(p5.width / 2, p5.height / 2)
-        p5.text('ASTEROIDS', 0, 0)
+        p5.text('ASTEROIDES', 0, 0)
         p5.textSize(32);
         p5.text(`LEVEL: ${this.level}`, 0, 100);
         p5.text(`PRESS A BUTTON TO START`, 0, 200);
