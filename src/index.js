ad5a3fd8edc408f283a1fa1c3ff919e23a97ecca
rubenkarlsson <ruben.karlsson@teclead-ventures.de>
2025-02-27 20:52:43 +0100

    save

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
-import p5 from 'p5';
-import './css/index.css';
+import p5 from "p5";
+import "p5/lib/addons/p5.sound";
+import "./css/index.css";
 
 // FONTS
-import font from './font/SpaceQuest-yOY3.ttf'
+import font from "./font/SpaceQuest-yOY3.ttf";
 
 // IMAGES
-import shipImage from './images/ship.png';
-import heartImage from './images/heart.png';
-import './images/favicon.ico';
+import shipImage from "./images/ship.png";
+import heartImage from "./images/heart.png";
+import "./images/favicon.ico";
 
 // GAME COMPONENTS
-import Background from './game/game-elements/background';
-import Game from './game/game';
-import { findOutHeight, findOutWidth } from './game/helpers';
+import Background from "./game/elements/background.js";
+import Game from "./game/game";
+import { findOutHeight, findOutWidth } from "./game/helpers";
+
+import SoundManager from "./game/soundManager.js";
 
 // global variables
 let background;
@@ -24,59 +27,61 @@ let spaceQuest;
 
 // p5 SKETCH
 export const Canvas = new p5((p5) => {
-    const resetSketch = (started, level, oldScore, oldLifes) => {
-        game = new Game(p5, started, level);
-        game.setup(ship, heart, oldScore, oldLifes);
-    }
+  let soundManager = new SoundManager(p5);
 
-    p5.preload = () => {
-        ship = p5.loadImage(shipImage);
-        heart = p5.loadImage(heartImage);
-        spaceQuest = p5.loadFont(font);
-    }
+  const resetSketch = (started, level, oldScore, oldLifes) => {
+    game = new Game(p5, soundManager, started, level);
+    game.setup(ship, heart, oldScore, oldLifes);
+  };
 
-    p5.setup = () => {
-        p5.createCanvas(findOutWidth(), findOutHeight());
-        p5.imageMode(p5.CENTER);
-        background = new Background(p5);
-        resetSketch(false, 1);
-        p5.textFont(spaceQuest);
-    }
+  p5.preload = () => {
+    ship = p5.loadImage(shipImage);
+    heart = p5.loadImage(heartImage);
+    spaceQuest = p5.loadFont(font);
+    soundManager.preload();
+    soundManager.addReverb();
+  };
 
-    p5.draw = () => {
-        background.draw();
-        game.draw();
+  p5.setup = () => {
+    p5.createCanvas(findOutWidth(), findOutHeight());
+    p5.imageMode(p5.CENTER);
+    background = new Background(p5);
+    resetSketch(false, 1);
+    p5.textFont(spaceQuest);
+  };
 
-        if (game.gameOver) {
-            p5.keyPressed = () => {
-                if (p5.keyCode === 32 || p5.keyCode === 13) {
-                    resetSketch(false, 1);
-                }
-            }
+  p5.draw = () => {
+    background.draw();
+    game.draw();
+
+    if (game.gameOver) {
+      p5.keyPressed = () => {
+        if (p5.keyCode === 32 || p5.keyCode === 13) {
+          resetSketch(false, 1);
         }
-
-        if (game.restartLevel) {
-            resetSketch(true, game.level, game.score, game.lifes)
-        }
-
-        let fps = p5.frameRate();
-        p5.fill(255);
-        p5.stroke(0);
-        p5.text("FPS: " + fps.toFixed(2), 10, p5.height - 10);
+      };
     }
 
-    p5.windowResized = () => {
-        p5.resizeCanvas(findOutWidth(), findOutHeight());
-        background = new Background(p5);
-    };
+    if (game.restartLevel) {
+      resetSketch(true, game.level, game.score, game.lifes);
+    }
 
+    let fps = p5.frameRate();
+    p5.fill(255);
+    p5.stroke(0);
+    p5.text("FPS: " + fps.toFixed(2), 10, p5.height - 10);
+  };
+
+  p5.windowResized = () => {
+    p5.resizeCanvas(findOutWidth(), findOutHeight());
+    background = new Background(p5);
+  };
 });
 
-
 window.top.document.onkeydown = function (evt) {
-    evt = evt || window.event;
-    var keyCode = evt.keyCode;
-    if (keyCode >= 37 && keyCode <= 40 || keyCode === 32) {
-        return false;
-    }
+  evt = evt || window.event;
+  var keyCode = evt.keyCode;
+  if ((keyCode >= 37 && keyCode <= 40) || keyCode === 32) {
+    return false;
+  }
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,47 @@
 // Generated using webpack-cli http://github.com/webpack-cli
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-    },
-    devServer: {
-        open: true,
-        host: 'localhost',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'index.html',
-            favicon: 'src/images/favicon.ico'
-        }),
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    open: true,
+    host: "localhost",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+      favicon: "src/images/favicon.ico",
+    }),
+    new webpack.ProvidePlugin({
+      p5: "p5",
+    }),
 
-        // Add your plugins here
-        // Learn more obout plugins from https://webpack.js.org/configuration/plugins/
+    // Add your plugins here
+    // Learn more obout plugins from https://webpack.js.org/configuration/plugins/
+  ],
+  module: {
+    rules: [
+      {
+        test: /\\.(js|jsx)$/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|jpeg|gif|ico|wav|ogg)$/,
+        type: "asset",
+      },
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
     ],
-    module: {
-        rules: [
-            {
-                test: /\\.(js|jsx)$/,
-                loader: 'babel-loader',
-            },
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|jpeg|gif|ico)$/,
-                type: 'asset',
-            },
-
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
-        ],
-    },
+  },
 };
